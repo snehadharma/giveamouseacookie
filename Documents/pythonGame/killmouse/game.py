3810f5e2ecb8adc97c1ddc80388d93929c93a344
Sneha Dharmalingam <sneha.dharma112@gmail.com>
2024-01-20 15:08:40 -0600

    finished step 4

--- Documents/pythonGame/killmouse/game.py
+++ Documents/pythonGame/killmouse/game.py
@@ -1,12 +1,21 @@
 import pygame
 
+from models import GameObject
 from utils import load_sprite
 
 class SpaceRocks:
     def __init__(self):
         self._init_pygame()
         self.screen = pygame.display.set_mode((800, 600))
-	self.background = load_sprite("space", False)
+        self.background = load_sprite("space", False)
+        self.clock = pygame.time.Clock()
+        self.spaceship = GameObject(
+            (400, 300), load_sprite("spaceship"), (0, 0)
+        )
+        self.asteroid = GameObject(
+            (400, 300), load_sprite("asteroid"), (1, 0)
+        )
+
     def main_loop(self):
         while True:
             self._handle_input()
@@ -18,16 +27,21 @@ class SpaceRocks:
         pygame.display.set_caption("Space Rocks")
 
     def _handle_input(self):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (
-            event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
-        ):
-            quit()
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+            ):
+                quit()
 
     def _process_game_logic(self):
-        pass
+        self.spaceship.move()
+        self.asteroid.move()
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
+        self.spaceship.draw(self.screen)
+        self.asteroid.draw(self.screen)
         pygame.display.flip()
+        self.clock.tick(60)
+
+
